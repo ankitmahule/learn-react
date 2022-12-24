b4d48babd6db51e6af0877d61998fd23e0f5641b
Ankit Mahule <ank.mahule@gmail.com>
2022-12-24 18:45:52 +0530

    Added JS and elements related code.

--- App.js
+++
@@ -1,3 +0,0 @@
-const heading = React.createElement('h1', {}, 'Namaste Everyone!');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
---
+++ Elements.js
@@ -0,0 +1,3 @@
+const header = React.createElement('h1', {id: 'heading1'}, 'Namaste Everyone!');
+const elements = ReactDOM.createRoot(document.getElementById('elements'));
+elements.render(header);
